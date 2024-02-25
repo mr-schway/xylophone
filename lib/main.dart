c4f5885eac8c315af6c4a44dad60fda6f6e8c65f
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2024-02-25 17:00:39 +0300

    Add buildKey function

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,20 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
-  void playSound(int soundNum) {
-    final player = AudioPlayer();
-    player.play(
-      AssetSource('assets_note$soundNum.wav'),
+  Expanded buildKey(Color color, int soundNum) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+        ),
+        onPressed: () {
+          final player = AudioPlayer();
+          player.play(
+            AssetSource('assets_note$soundNum.wav'),
+          );
+        },
+        child: const Text(''),
+      ),
     );
   }
 
@@ -19,72 +29,18 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.red,
-                ),
-                onPressed: () {
-                  playSound(1);
-                },
-                child: const Text(''),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.orange,
-                ),
-                onPressed: () {
-                  playSound(2);
-                },
-                child: const Text(''),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.yellow,
-                ),
-                onPressed: () {
-                  playSound(3);
-                },
-                child: const Text(''),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.green,
-                ),
-                onPressed: () {
-                  playSound(4);
-                },
-                child: const Text(''),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                ),
-                onPressed: () {
-                  playSound(5);
-                },
-                child: const Text(''),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.indigo,
-                ),
-                onPressed: () {
-                  playSound(6);
-                },
-                child: const Text(''),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.purple,
-                ),
-                onPressed: () {
-                  playSound(7);
-                },
-                child: const Text(''),
-              ),
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.blue, 5),
+              buildKey(Colors.indigo, 6),
+              buildKey(Colors.purple, 7),
             ],
           ),
         ),
